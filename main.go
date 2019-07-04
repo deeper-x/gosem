d45f0e727e1d5c847b1231140522e597634fe0b4
deeper-x <albertodeprezzo@gmail.com>
2019-07-04 18:33:49 +0200

    defining main functions reqs

--- main.go
+++ main.go
@@ -6,29 +6,43 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 type semVerFile struct {
 	name  string
 	regex string
+	value []byte
 }
+
+// 1R-GV: GEN VERSION
+// 1. launch bin: get git tag and write it to file
+// 1A. get git describe value
+// 1B. overwrite it in file
+
+// 2R-UV: UPDATE Maj-Min-Patch
+// 1. Read version file. Get a slice []string [maj, min, patch]
+// 2. Create an updateMaj func: increment slice[0] and update file
+// 3. Create an updateMin func: increment slice[1] and update file
+// 4. Create an updatePatch func: increment slice[2] and update file
 
 func main() {
 	inFile := semVerFile{
-		name:  "<PATH>",
-		regex: `^v[0-9]+\.[0-9]+\.[0-9]+`,
+		name:  "./build_version.html",
+		regex: `^v\.[0-9]+\.[0-9]+\.[0-9]+`,
 	}
 
-	//semVerTag := genSemVerTag()
-	curSemVer := inFile.readCurSemVer()
-	//inFile.writeSemVer(semVerTag)
-	itIs := inFile.isSemVer(string(curSemVer))
+	// 1R-GV
+	var semVerB []byte = inFile.genGitTag()
+	inFile.writeSemVer(semVerB)
 
-	if !itIs {
-		fmt.Println(string(curSemVer), "is not")
-	} else {
-		fmt.Println("it is")
-	}
+	/* 	// 2R-UV
+	   	var fileContent []byte = inFile.readCurSemVer()
+	   	var stringContent = string(fileContent)
+
+	   	var sliceContent = strings.Split(stringContent, ".")
+	   	fmt.Println(sliceContent) */
+
 }
 
 func (inFile semVerFile) isSemVer(inString string) bool {
@@ -42,12 +56,20 @@ func (inFile semVerFile) isSemVer(inString string) bool {
 	return matched
 }
 
+func (inFile semVerFile) getLabels() []string {
+	retSlice := strings.Split(string(inFile.value), ".")
+
+	return retSlice
+
+}
+
 func (inFile semVerFile) writeSemVer(fileContent []byte) {
 	ioutil.WriteFile(inFile.name, fileContent, 0644)
 }
 
 func (inFile semVerFile) readCurSemVer() []byte {
 	data, err := ioutil.ReadFile(inFile.name)
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
@@ -56,11 +78,17 @@ func (inFile semVerFile) readCurSemVer() []byte {
 	return data
 }
 
-func genSemVerTag() []byte {
-	cmd := exec.Command("git", "describe")
+func (inFile *semVerFile) assignSemVer() {
+	value := inFile.readCurSemVer()
+
+	(*inFile).value = value
+}
+
+func (inFile semVerFile) genGitTag() []byte {
+	cmd := exec.Command("git", "describe --tags")
 
 	out, err := cmd.Output()
-
+	fmt.Println(out)
 	if err != nil {
 		fmt.Println(err)
 	}
