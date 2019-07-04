4efed70a8c88fddbf4f73b11af1dc2e3fdb60c1e
deeper-x <albertodeprezzo@gmail.com>
2019-07-04 11:12:12 +0200

    WIP: added core funcs to manage r/w on file

--- main.go
+++ main.go
@@ -3,21 +3,60 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
+	"regexp"
 )
 
-var semverFile = "<PATH>"
+type semVerFile struct {
+	name  string
+	regex string
+}
 
 func main() {
-	semVer := genSemVer()
-	writeSemVer(semVer)
+	inFile := semVerFile{
+		name:  "<PATH>",
+		regex: `^v[0-9]+\.[0-9]+\.[0-9]+`,
+	}
+
+	//semVerTag := genSemVerTag()
+	curSemVer := inFile.readCurSemVer()
+	//inFile.writeSemVer(semVerTag)
+	itIs := inFile.isSemVer(string(curSemVer))
+
+	if !itIs {
+		fmt.Println(string(curSemVer), "is not")
+	} else {
+		fmt.Println("it is")
+	}
 }
 
-func writeSemVer(fileContent []byte) {
-	ioutil.WriteFile(semverFile, fileContent, 0644)
+func (inFile semVerFile) isSemVer(inString string) bool {
+	matched, err := regexp.Match(inFile.regex, []byte(inString))
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return matched
 }
 
-func genSemVer() []byte {
+func (inFile semVerFile) writeSemVer(fileContent []byte) {
+	ioutil.WriteFile(inFile.name, fileContent, 0644)
+}
+
+func (inFile semVerFile) readCurSemVer() []byte {
+	data, err := ioutil.ReadFile(inFile.name)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	return data
+}
+
+func genSemVerTag() []byte {
 	cmd := exec.Command("git", "describe")
 
 	out, err := cmd.Output()
